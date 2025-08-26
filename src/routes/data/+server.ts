d45ff7ab60589c3b42c68b9f6de840e5f6ed63d6
cubedhuang <99danielh@gmail.com>
2025-08-26 02:19:53 -0400

    use google api instead of csv link

--- src/routes/data/+server.ts
+++ src/routes/data/+server.ts
@@ -1,10 +1,10 @@
 import { json } from '@sveltejs/kit';
+import { GOOGLE_KEY } from '$env/static/private';
 import type { Word } from '$lib/types';
 import { sortAlphabetically } from '$lib/util.js';
-import parser from 'papaparse';
+import { sheets_v4 } from 'googleapis/build/src/apis/sheets/v4';
 
-const SHEET_URL =
-	'https://docs.google.com/spreadsheets/d/e/2PACX-1vTVGXFd17kcvfu__zjshqiV3kW360IclOEfEdWda_K6ZCg4TY6nW2Gwn4_bs1yQeFLwrZI1_xEvSuP0/pub?gid=0&single=true&output=csv';
+const SHEET_ID = '1SiXXmQUrQ2KI7SRV3Hp_FP53HDteuuMF1ZJ_d_AwBr4';
 
 const keys: Record<string, keyof Word> = {
 	Word: 'word',
@@ -21,17 +21,28 @@ const keys: Record<string, keyof Word> = {
 };
 
 export async function GET({ fetch, setHeaders }) {
-	const text = await fetch(SHEET_URL).then(res => res.text());
+	const sheets = new sheets_v4.Sheets({
+		auth: GOOGLE_KEY,
+		fetchImplementation: fetch
+	});
+	const res = await sheets.spreadsheets.values.get({
+		spreadsheetId: SHEET_ID,
+		range: 'Dictionary'
+	});
 
-	const { data } = parser.parse(text, { header: true }) as {
-		data: Record<string, string>[];
-	};
+	if (!res.data.values) {
+		return json([]);
+	}
 
-	const words = data
+	const firstRow = res.data.values.shift() as string[];
+
+	const words = res.data.values
+		.slice(1)
 		.map(word => {
 			const w = {} as Word;
-			for (const key of Object.keys(keys)) {
-				w[keys[key] as 'word'] = word[key];
+			for (let i = 0; i < word.length; i++) {
+				const key = keys[firstRow[i]];
+				w[key] = word[i];
 			}
 			return w;
 		})
